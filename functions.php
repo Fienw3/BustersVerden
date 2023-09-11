d2d79ac7cca2336753de9f42d25f811e3ef5f8cf
Fienw3 <annwal01@easv365.dk>
2023-09-11 11:50:30 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes( ) ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo("name"); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <?php wp_head(); ?>
+</head>
+
+
+
+<body>
+
+    <header>
