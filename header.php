5fa69e48791fffb0fc84c94b9ca74fa0acbea648
Fienw3 <annwal01@easv365.dk>
2023-09-14 09:48:03 +0200

    Meta tags inserted

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Skarntyden Esbjerg">
     <title><?php bloginfo("name"); ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <?php wp_head(); ?>
